25c9dbf96716ff66a93f166aeee7fdd6a949d6ec
alan <101748867+AlanTXM@users.noreply.github.com>
2023-04-17 13:27:13 -0300

    feat: layout changes

--- frontend/src/app/modals/user-modal/user-modal.component.ts
+++ frontend/src/app/modals/user-modal/user-modal.component.ts
@@ -6,6 +6,7 @@ import { HttpService } from 'src/services/http.service';
   templateUrl: './user-modal.component.html',
   styleUrls: ['./user-modal.component.scss']
 })
+
 export class UserModalComponent implements OnInit {
 
   constructor(private httpService : HttpService) { }
