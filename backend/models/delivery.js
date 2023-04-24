7cae846959ff9035c85b62a9ea923917b6b75f15
alan <101748867+AlanTXM@users.noreply.github.com>
2023-04-24 18:57:46 -0300

    calcular frete backend

---
+++ backend/models/delivery.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define('delivery', {
+        id : {
+            type : Sequelize.INTEGER.UNSIGNED,
+            primaryKey : true,
+            autoIncrement : true,
+            allowNull : false
+        },
+        fkProduct : {
+            type : Sequelize.INTEGER(),
+            allowNull : false,
+        },
+        price : {
+            type : Sequelize.INTEGER(),
+            allowNull : false,
+        },
+        porcent : {
+            type : Sequelize.INTEGER(),
+            allowNull : false,
+        },
+        status : {
+            type : Sequelize.INTEGER,
+            allowNull : false
+        },
+    })
+}
